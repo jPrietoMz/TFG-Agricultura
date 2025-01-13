c48e310bab7ddb85f70f5690efce68e82feca1d1
jPrietoMz <JPRIETOM392@alumnos.imf.com>
2025-01-13 15:31:23 +0100

    Primer paso. Creación backend

---
+++ backend/agricultura-app-backend/src/test/java/com/agricultura/agricultura_app_backend/AgriculturaAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agricultura.agricultura_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriculturaAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
