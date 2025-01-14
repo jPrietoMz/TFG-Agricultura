9f18695ece18a1f393b02226abea90b125dbae3d
jPrietoMz <JPRIETOM392@alumnos.imf.com>
2025-01-14 13:39:55 +0100

    Backend (0.0.1) Spring Boot

---
+++ backend/agricultura-backend/src/main/java/com/tfg/agricultura/agricultura_backend/AgriculturaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.agricultura.agricultura_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriculturaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/agricultura-backend/src/test/java/com/tfg/agricultura/agricultura_backend/AgriculturaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.agricultura.agricultura_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriculturaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
